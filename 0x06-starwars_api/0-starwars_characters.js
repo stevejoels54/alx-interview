e6de08bf67738ed9c257ed733b4d04a31e9e13df
joelsteven <joelofelectronics@gmail.com>
2024-02-16 02:29:46 +0300

    Fix: Star Wars Characters async/await

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,21 +4,24 @@ const request = require('request');
 const api = 'https://swapi-api.hbtn.io/api';
 const id = process.argv[2];
 
-request(`${api}/films/${id}`, (error, response, body) => {
+request(`${api}/films/${id}`, async (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
-  // get the characters
+
   const characters = JSON.parse(body).characters;
-  // print each character
-  characters.forEach((url) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-      console.log(JSON.parse(body).name);
+
+  for (const character of characters) {
+    await new Promise((resolve, reject) => {
+      request(character, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve(body);
+        }
+      });
     });
-  });
+  }
 });
